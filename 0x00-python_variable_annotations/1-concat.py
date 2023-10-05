0207046e61cb11c0f11e9701f2efd13b27a25647
Michael Appiah Dankwah <117749434+Terre8055@users.noreply.github.com>
2023-10-05 22:19:25 +0000

    function concat that takes a string str1 and a string str2

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module to return concatenated strings"""
+
+def concat(str1: str, str2: str)-> str:
+    """Function to concat two strings
+
+    Args:
+        str1 (str): paramA
+        str2 (str): paramB
+
+    Returns:
+        str: concat string
+    """
+    return str1 + str2
