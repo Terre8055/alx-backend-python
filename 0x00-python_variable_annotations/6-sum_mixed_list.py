10e3b3487029b42e9b22d8bec3220e0d9ed2d113
Michael Appiah Dankwah <117749434+Terre8055@users.noreply.github.com>
2023-10-05 23:04:37 +0000

    sum mixed list

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 """Module to return sum of list of a int as float"""
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[int]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Return float of list"""
     init_fig: float = 0
     for i in mxd_lst:
         init_fig += i
     return init_fig
+
+
+print(sum_mixed_list.__annotations__)
