a9a5c142ad82886cc8458afe7519cddf0c2193ee
Michael Appiah Dankwah <117749434+Terre8055@users.noreply.github.com>
2023-10-05 22:11:10 +0000

    Type annotation

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module to return sum of two floats"""
+
+def add (a: float, b: float) -> float:
+    """Add two parameters
+
+    Args:
+        a (float): param A
+        b (float): param B
+
+    Returns:
+        float: return type
+    """
+    return a + b
+
