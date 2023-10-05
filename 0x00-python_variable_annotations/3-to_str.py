6b2c7a6c297e1528cf3c13605936063d88e8ec2e
Michael Appiah Dankwah <117749434+Terre8055@users.noreply.github.com>
2023-10-05 22:37:02 +0000

    String repr

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module to return string representation of a float"""
+
+
+def to_str(n: float) -> str:
+    """Return string representation of a float"""
+    return str(float)
