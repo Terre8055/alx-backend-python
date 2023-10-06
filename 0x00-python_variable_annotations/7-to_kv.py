ea69186b88f823d787e429fdda5c090ccdff1b5f
Michael Appiah Dankwah <117749434+Terre8055@users.noreply.github.com>
2023-10-06 09:06:42 +0000

    tuple return

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Type-annotated function to_kv that takes a string k and an int OR float v as arguments and returns a tuple"""
+"""Module to return complex types"""
 from typing import Union, Tuple
 
 
